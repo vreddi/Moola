88d69e7c06cf6e083c8508e5c5da35d293f5c28b
Vishrut Reddi <visre@microsoft.com>
2016-06-21 15:20:26 -0700

    Functionality Extended for EntityManager

--- Controller/EntityManager.ts
+++ Controller/EntityManager.ts
@@ -79,4 +79,115 @@ export class EntityManager{
         this.Entities.sort();
         this.Entities.reverse();
     }
+
+    /**
+     * Accross the collection of entities this function gets the total number of days
+     * involved in that collection.
+     * @returns: Total number of days (number)
+     */
+    public getTotalDays(){
+
+        // 0 is no day or month or year. 0 here is used as placeholder
+        var currDayNum : number = 0;
+        var currMonthNum : number = 0;
+        var currYearNum : number = 0;
+        var dayCount : number = 0;
+
+        for(var index = 0; index < this.Entities.count(); index++){
+
+            var entity : Entity.Entity = this.Entities.getItem(index);
+
+            // In case of invalid day, dont count the entity
+            if(entity.date.dayNumber == null || isNaN(entity.date.dayNumber) || entity.date.dayNumber == undefined){
+                continue;
+            }
+
+            // In case of invalid month, dont count the entity
+            if(entity.date.monthNumber == null || isNaN(entity.date.monthNumber) || entity.date.monthNumber == undefined){
+                continue;
+            }
+
+            // In case of invalid year, dont count the entity
+            if(entity.date.yearNumber == null || isNaN(entity.date.yearNumber) || entity.date.yearNumber == undefined){
+                continue;
+            }
+
+            if(currDayNum != entity.date.dayNumber || currMonthNum != entity.date.monthNumber || currYearNum != entity.date.yearNumber){
+
+                dayCount++;
+                currDayNum = entity.date.dayNumber;
+                currMonthNum = entity.date.monthNumber;
+                currYearNum = entity.date.yearNumber;
+            }
+        }
+
+        return dayCount;
+    }
+
+    /**
+     * Accross the collection of entities this function gets the total number of months
+     * involved in that collection.
+     * @returns: Total number of months (number)
+     */
+    public getTotalMonths(){
+        
+        // 0 is no month or year. 0 here is used as placeholder
+        var currMonthNum : number = 0;
+        var currYearNum : number = 0;
+        var monthCount : number = 0;
+
+        for(var index = 0; index < this.Entities.count(); index++){
+
+            var entity : Entity.Entity = this.Entities.getItem(index);
+
+            // In case of invalid month, dont count the entity
+            if(entity.date.monthNumber == null || isNaN(entity.date.monthNumber) || entity.date.monthNumber == undefined){
+                continue;
+            }
+
+            // In case of invalid year, dont count the entity
+            if(entity.date.yearNumber == null || isNaN(entity.date.yearNumber) || entity.date.yearNumber == undefined){
+                continue;
+            }
+
+            if(currMonthNum != entity.date.monthNumber || currYearNum != entity.date.yearNumber){
+
+                monthCount++;
+                currMonthNum = entity.date.monthNumber;
+                currYearNum = entity.date.yearNumber;
+            }
+        }
+
+        return monthCount;
+    }
+
+    /**
+     * Accross the collection of entities this function gets the total number of years
+     * involved in that collection.
+     * @returns: Total number of years (number)
+     */
+    public getTotalYears(){
+
+        // 0 is no year. 0 here is used as placeholder
+        var currYearNum : number = 0;
+        var yearCount = 0;
+
+        for(var index = 0; index < this.Entities.count(); index++){
+
+            var entity : Entity.Entity = this.Entities.getItem(index);
+
+            // In case of invalid year, dont count the entity
+            if(entity.date.yearNumber == null || isNaN(entity.date.yearNumber) || entity.date.yearNumber == undefined){
+                continue;
+            }
+
+            if(currYearNum != entity.date.yearNumber){
+
+                yearCount++;
+                currYearNum = entity.date.yearNumber;
+            }
+        }
+
+        return yearCount;
+    }
 }
