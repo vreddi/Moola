8c8b71db8c0abf33fca23966fc0ff3a318461b80
Vishrut Reddi <vishrutreddi@gmail.com>
2016-07-29 21:35:42 -0700

    Adding main.js file

---
+++ Script/main.js
@@ -0,0 +1,26 @@
+//RequireJS Configuration
+requirejs.config({
+    //Where paths are evaluated from
+    baseUrl: 'Script',
+
+    paths: {
+
+		jquery: 'External/jquery-2.2.4.min',
+		d3: 'External/d3.min',
+		//Moola modules
+		Date: 'Model/Date',
+		Cost: 'Model/Cost',
+		Flow: 'Model/Flow',
+		PaymentMethod: 'Model/PaymentMethod',
+		Item: 'Model/Item',
+		Tags: 'Model/Tags',
+		Entity: 'Model/Entity',
+		Parser: 'Model/Parser',
+		Collections: 'Model/Collections',
+		Constants: 'Model/Constants',
+		Visualizer: 'Model/Visualizer',
+		EntityManager: 'Controller/EntityManager',
+		"../Controller/EntityManager" : 'Controller/EntityManager',
+		BarChart: 'Model/BarChart'
+    }
+});
