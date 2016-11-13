bafcf56e60b7060feba96d2656e6d8afa820c080
Vishrut Reddi <vishrutreddi@gmail.com>
2016-11-13 00:02:08 -0800

    Added some new Models

---
+++ Script/Model/MonthlyFinanceEntity.js
@@ -0,0 +1,42 @@
+define(["require", "exports", "./Constants"], function (require, exports, Constants_1) {
+    "use strict";
+    var MonthlyFinanceEntity = (function () {
+        function MonthlyFinanceEntity() {
+        }
+        MonthlyFinanceEntity.CreateMonthlyFinances = function (entityManager, year) {
+            var constantsLibrary = new Constants_1.Constants(), months = constantsLibrary.constants["months"], allMonthlyFinances = [], totalEntities = entityManager.Entities.count();
+            for (var index = 0; index < months.length; index++) {
+                var monthFinanceEntity = new MonthlyFinanceEntity();
+                monthFinanceEntity.monthNumber = index + 1;
+                monthFinanceEntity.monthName = months[index];
+                monthFinanceEntity.expenditure = 0;
+                monthFinanceEntity.earning = 0;
+                monthFinanceEntity.yearNumber = year;
+                allMonthlyFinances.push(monthFinanceEntity);
+            }
+            for (var index = 0; index < totalEntities; index++) {
+                if (entityManager.Entities.getItem(index) != undefined && entityManager.Entities.getItem(index).date.yearNumber != year) {
+                    continue;
+                }
+                else {
+                    var partialMonthFinanceEntity = new MonthlyFinanceEntity();
+                    partialMonthFinanceEntity.monthNumber = entityManager.Entities.getItem(index).date.monthNumber;
+                    if (entityManager.Entities.getItem(index).IsEarning()) {
+                        partialMonthFinanceEntity.earning = entityManager.Entities.getItem(index).cost.value;
+                        partialMonthFinanceEntity.expenditure = 0;
+                    }
+                    else {
+                        partialMonthFinanceEntity.earning = 0;
+                        partialMonthFinanceEntity.expenditure = entityManager.Entities.getItem(index).cost.value;
+                    }
+                    allMonthlyFinances[partialMonthFinanceEntity.monthNumber - 1].earning += partialMonthFinanceEntity.earning;
+                    allMonthlyFinances[partialMonthFinanceEntity.monthNumber - 1].expenditure += partialMonthFinanceEntity.expenditure;
+                }
+            }
+            return allMonthlyFinances;
+        };
+        return MonthlyFinanceEntity;
+    }());
+    exports.MonthlyFinanceEntity = MonthlyFinanceEntity;
+});
+//# sourceMappingURL=MonthlyFinanceEntity.js.map
