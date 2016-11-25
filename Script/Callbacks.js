4b3cf591c5754131829bede408e9548e20c1fb2b
Vishrut Reddi <vishrutreddi@gmail.com>
2016-11-25 02:27:15 -0800

    Fix: Global Variables now accessible in Developer Tools
    * Accessing values through the Windows object
    * This is temporary.

--- Script/Callbacks.js
+++ Script/Callbacks.js
@@ -1,8 +1,8 @@
 console.log('Hello Developer!');
-var v = "";
-var p = "";
-var a = "";
-var $ = require('jquery');
+window.v = "";
+window.p = "";
+window.a = "";
+window.$ = require('jquery');
 var d3 = require('d3');
 var c3 = require('c3');
 var Visualizer = require('Visualizer');
@@ -16,21 +16,18 @@ function clearScreen(){
     $('body .content').empty();
 }
 
-$(document).ready(function($) {
+window.$(document).ready(function($) {
 
     // Callback for uploading a csv file to parse
     document.getElementById("upload").addEventListener('change', function(){
         var fr = new FileReader();
         fr.onload = function(){
-            p = new Parser.Parser();
-            v = new Visualizer.Visualizer(this.result, d3, c3, $);
+            window.p = new Parser.Parser();
+            window.v = new Visualizer.Visualizer(this.result, d3, c3, $);
 
-            console.log();
-            console.log('Visualizer: (Store as global variable in developer tools for debigging)')
-            console.log(v);
             //Used for debugging on console for now
             // Remove this later
-            a = p.CsvToJson(this.result);
+            window.a = window.p.CsvToJson(this.result);
         }
         fr.readAsText(this.files[0]);
         clearScreen();
